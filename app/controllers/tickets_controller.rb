379bdc140d3fdacd7b855b6fe628b631c1171063
Sciborowska Basia <the-tjorven@hotmail.com>
2015-05-12 13:14:16 +0000

    add_ticket

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,12 @@
+class TicketsController < ApplicationController
+  def create
+    @project = Project.find(params[:project_id])
+    @ticket = @project.tickets.create(ticket_params)
+    redirect_to project_path(@project)
+  end
+ 
+  private
+  def ticket_params
+    params.require(:ticket).permit(:title, :description, :priority, :difficulty, :status)
+    end
+end
