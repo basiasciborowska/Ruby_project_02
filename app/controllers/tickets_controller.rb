36d26d7fbbaa0258804210a62787a6199d0ad669
Sciborowska Basia <the-tjorven@hotmail.com>
2015-05-12 13:45:52 +0000

    ticket_index

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,6 +1,18 @@
 class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all
+  end
+  
+  def show
+    @ticket = Ticket.find(params[:id])
+  end
   
   def new
+    @ticket = Ticket.new
+  end
+  
+  def edit
+    @ticket = Ticket.find(params[:id])
   end
   
   def create
