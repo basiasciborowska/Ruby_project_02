f116d4090c3304115a22442a88fe7b5bbbee46bb
Sciborowska Basia <the-tjorven@hotmail.com>
2015-05-21 20:32:09 +0000

    remove_attachment

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,12 @@ class Ticket < ActiveRecord::Base
   validates_attachment_content_type :picture, :content_type => /\Aimage\/.*\Z/
   validates_attachment_file_name :picture, :matches => [/gif\Z/, /png\Z/, /jpe?g\Z/, /doc\Z/, /xls\Z/, /ppt\Z/]
   validates_with AttachmentSizeValidator, :attributes => :picture, :less_than => 1.megabytes
+  attr_accessor :remove_picture
+  
+  before_save :delete_picture, if: ->{ remove_picture == '1' && !picture_updated_at_changed? }
+  
+  private
+    def delete_picture
+      self.picture = nil
+    end
 end
